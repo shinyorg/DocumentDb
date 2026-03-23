eec4de24a4c4a4055f75fc2c85f8534ebacd21ee
Allan Ritchie <allan.ritchie@gmail.com>
2026-03-23 18:34:00 -0400

    Missed library

---
+++ src/Shiny.DocumentDb.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Shiny.DocumentDb;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDocumentStore(this IServiceCollection services, Action<DocumentStoreOptions> configure)
+    {
+        var options = new DocumentStoreOptions
+        {
+            DatabaseProvider = null!
+        };
+        configure(options);
+
+        if (options.DatabaseProvider == null)
+            throw new ArgumentException("DatabaseProvider is null must be set.", nameof(configure));
+
+        services.AddSingleton(options);
+        services.AddSingleton<IDocumentStore, DocumentStore>();
+        return services;
+    }
+}
